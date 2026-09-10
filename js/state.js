98930a68e6112f9ecd4693c61370808bef5bcb35
krtschil <klaus@krtschil.net>
2026-09-11 00:11:28 +0200

    state.js

---
+++ js/state.js
@@ -0,0 +1,13 @@
+window.appState = {
+    language: "de",
+    hands: null,
+    lastBoardIndex: 0,
+    session: 0,
+    currentTraveller: null,
+    handEntryMode: false,
+    inputDirection: 0,
+    currentBoard: null,
+    currentPlayer: null,
+    currentContract: null,
+    currentDeclarer: null
+};

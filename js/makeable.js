1e6371e4ba1218890aa7d9af5481aa67fdee179a
krtschil <klaus@krtschil.net>
2026-09-11 14:20:51 +0200

    Next steps

--- js/makeable.js
+++ js/makeable.js
@@ -109,39 +109,3 @@ function getMakeableTricksForLead(pindex,tline)
 	return ntricks;
 }
 
-function getBoardIndex(travindex)
-{
-	// get board index, given index to a traveller.
-	var board = g_travellers.event.board[travindex].board_no;
-
-	var i;
-
-	for (i=0;i<g_hands.boards.length;i++)
-	{
-		if (g_hands.boards[i].board == board)
-		{
-			return i;
-		}
-	}
-
-	return null;
-}
-
-function getTravIndex(boardindex)
-{
-	// get traveller index, given index to a board.
-	var board = g_hands.boards[boardindex].board;
-
-	var i;
-
-	for (i=0;i<g_travellers.event.board.length;i++)
-	{
-		if (g_travellers.event.board[i].board_no == board)
-		{
-			return i;
-		}
-	}
-
-	return null;
-}
-

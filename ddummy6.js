5464e8665dad4ed1f759923a1a686460388ebf51
krtschil <klaus@krtschil.net>
2026-09-11 14:22:29 +0200

    Next steps

--- ddummy6.js
+++ ddummy6.js
@@ -7032,42 +7032,6 @@ function setBars(cells,value,cellindex,range,colorL,colorR,width)
 	cells[cellindex].style.borderLeft="1px solid #CCCCCC";
 }
 
-function getBoardIndex(travindex)
-{
-	// get board index, given index to a traveller.
-	var board = g_travellers.event.board[travindex].board_no;
-
-	var i;
-
-	for (i=0;i<g_hands.boards.length;i++)
-	{
-		if (g_hands.boards[i].board == board)
-		{
-			return i;
-		}
-	}
-
-	return null;
-}
-
-function getTravIndex(boardindex)
-{
-	// get traveller index, given index to a board.
-	var board = g_hands.boards[boardindex].board;
-
-	var i;
-
-	for (i=0;i<g_travellers.event.board.length;i++)
-	{
-		if (g_travellers.event.board[i].board_no == board)
-		{
-			return i;
-		}
-	}
-
-	return null;
-}
-
 function addSummaryRow(stable,text)
 {
 	stable.insertRow(-1);

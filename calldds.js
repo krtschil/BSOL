2e1f65b8f7958217808e24adfb621ce50da0dddc
krtschil <klaus@krtschil.net>
2026-09-11 13:27:50 +0200

    Next steps

--- calldds.js
+++ calldds.js
@@ -225,21 +225,7 @@ var Module = {
  };
   
 importScripts("dds.js");
-
-function convertVulStr(vulstr)
-{
-	var vul = ["None","All","NS","EW"];
-	
-	vulstr = vulstr.toUpperCase();
-	
-	for (var i=0;i<vul.length;i++)
-	{
-		if (vul[i].toUpperCase()==vulstr)
-			return i;
-	}
-	
-	return -1;
-}
+importScripts("js/shared/bridge-utils.js");
 
 function getBestScore(msg)
 {
--- js/scoring.js
+++ js/scoring.js
@@ -61,21 +61,6 @@ function convertParContractString(str)
 	return result;
 }
 
-function convertVulStr(vulstr)
-{
-	var vul = ["None","All","NS","EW"];
-
-	vulstr = vulstr.toUpperCase();
-
-	for (var i=0;i<vul.length;i++)
-	{
-		if (vul[i].toUpperCase()==vulstr)
-			return i;
-	}
-
-	return -1;
-}
-
 function getContractType(contract)
 {
 	var result = {};

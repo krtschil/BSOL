827d224306068309f7a0de64fdaed5e34ad54df0
krtschil <klaus@krtschil.net>
2026-09-11 14:11:53 +0200

    Next steps

--- ddummy6.js
+++ ddummy6.js
@@ -5390,62 +5390,6 @@ function setOptions(optionsStr)
 
 
 
-function getInfoForSimilarContracts(lineIndex,direction)
-{
-	var i;
-	var traveller = g_currentTraveller.traveller_line;
-	var curLine = traveller[lineIndex];
-	var suit = curLine.contract.charAt(1);
-	var declarer = curLine.played_by;
-	var level = Number(curLine.contract.charAt(0));
-
-	var result = {};
-	result.totalPairs = traveller.length;
-	result.totalThisSuitAndDeclarer = 0;
-	result.moreTricks = 0;
-	result.sameTricks = 0;
-	result.lessTricks = 0;
-	result.bidPartScore = 0;
-	result.madePartScore = 0;
-	result.bidGameScore = 0;
-	result.madeGameScore = 0;
-	result.bidSmallSlam = 0;
-	result.madeSmallSlam = 0;
-	result.bidGrandSlam = 0;
-	result.madeGrandSlam = 0;
-
-	for (i=0;i<traveller.length;i++)
-	{
-		if (i!=lineIndex)
-		{
-			var t = traveller[i];
-
-			if ((t.contract.charAt(1)==suit)&&(declarer==t.played_by))
-			{
-				result.totalThisSuitAndDeclarer++;
-
-
-			}
-		}
-	}
-
-	var playedByPercent = Math.round((100*(result.totalThisSuitAndDeclarer+1))/(result.totalPairs));
-
-	switch(language)
-	{
-		case "de":
-			var str = "Diese Farbe/Alleinspieler-Kombination wurde an " + result.totalThisSuitAndDeclarer + " von " + result.totalPairs + " anderen Tischen (" + playedByPercent + "%).";
-			break;
-		default:
-			var str = "This suit/declarer combination was played at " + result.totalThisSuitAndDeclarer + " of " + result.totalPairs + " other tables (" + playedByPercent + "%).";
-	}
-	
-	const clean = DOMPurify.sanitize(document.getElementById("comparisonText").innerHTML + "<span style=\"font-size:12px;\"><br><br><p>" + str + "</p></span>", { RETURN_DOM_FRAGMENT: true });
-	document.getElementById("comparisonText").replaceChildren(clean);
-	//document.getElementById("comparisonText").innerHTML = document.getElementById("comparisonText").innerHTML + "<span style=\"font-size:12px;\"><br><br><p>" + str + "</p></span>";
-
-	return result;
-}
 
 function getMakeableTricksForContract(index,contract,declarer)
 {

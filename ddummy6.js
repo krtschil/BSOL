40e5c07b007535e6cf746c949cd776ad80f7aaf9
krtschil <klaus@krtschil.net>
2026-09-11 19:36:04 +0200

    Next steps

--- ddummy6.js
+++ ddummy6.js
@@ -1973,15 +1973,6 @@ function showTravellerRowButtons()
 	return false;
 }
 
-function exitCardPlay()
-{
-	resetTimeout();
-	terminateSession();
-	document.getElementById("play").innerHTML = g_playButtonText;
-	setupTraveller(g_lastBindex,true);
-	enterPlayMode();
-	document.getElementById("mctable").className = "";
-}
 
 function setupTraveller(index,active)
 {
@@ -4665,51 +4656,7 @@ function dddloadfunc(data,statusText,jqXHR,context)
 	}
 }
 
-function playNextCard(pthis)
-{
-	if (g_showPlay)
-	{
-		if ((typeof g_hands.boards[g_lastBindex].Played)!="undefined")
-		{
-			var played = g_hands.boards[g_lastBindex].Played;
 
-			if (g_currentPlayIndex<played.length)
-			{
-				if (g_lastMatchedPlayIndex == (g_currentPlayIndex - 1))	// still following the actual cards played in the match
-				{
-					var playedInMatch = played[g_currentPlayIndex];
-
-					if (requestPending())
-						return;	// Don't allow while there is a request in progress.
-					else
-						setRequestTimeout(true);
-
-					spinner(pthis);
-					callddd(playedInMatch.toUpperCase());
-				}
-				else
-				{
-					displayError(document.getElementById("boardNumber"),"<span style=\"font-size:18px;background-color:#FFFFEE;padding:10px;border:1px solid black;\">Es wird momentan nicht dem Abspiel gefolgt</span>");
-				}
-			}
-			else if (g_currentPlayIndex==played.length)
-			{
-				if (g_lastMatchedPlayIndex == (g_currentPlayIndex - 1))
-				{
-					displayError(document.getElementById("boardNumber"),"<span style=\"font-size:18px;background-color:#FFFFEE;padding:10px;border:1px solid black;\">Keine weiteren gespielten Karten vorhanden</span>");
-				}
-				else
-				{
-					displayError(document.getElementById("boardNumber"),"<span style=\"font-size:18px;background-color:#FFFFEE;padding:10px;border:1px solid black;\">Es wird momentan nicht dem Abspiel gefolgt</span>");
-				}
-			}
-			else
-			{
-				displayError(document.getElementById("boardNumber"),"<span style=\"font-size:18px;background-color:#FFFFEE;padding:10px;border:1px solid black;\">Keine weiteren gespielten Karten vorhanden</span>");
-			}
-		}
-	}
-}
 
 function hideForwardPlay()
 {
@@ -4728,84 +4675,7 @@ function showForwardPlay()
 	} catch (err) {};
 }
 
-function enterPlayMode()
-{
-	setMode(1);	// indicate page is in play g_mode
-	show("editHand");
-	hide("backPlay");
-	hideForwardPlay();
-	show("play");
-	$("#scorecard").hide();
-	show("help");
 
-	document.getElementById("makeableContracts").className = "mc_large";
-	document.getElementById("traveller").style.width = "800px";
-	document.getElementById("northHand").style.height = Math.floor(g_sectionHeight) + "px";
-	document.getElementById("westHand").style.height = Math.floor(g_sectionHeight) + "px";
-	document.getElementById("southHand").style.height = Math.floor(g_sectionHeight) + "px";
-	document.getElementById("wvul").style.height = (g_sectionHeight-40) + "px";	// Increase height of vulnerability bar to match new table height.
-	document.getElementById("vul").style.width = g_sectionHeight + "px";	// Increase width of table centre to match new table height.
-
-	var contracts = document.getElementById("makeableContracts");
-	var rows = contracts.rows;
-
-	var cvector = g_hands.boards[g_lastBindex].DoubleDummyTricks;
-	var value;
-
-	for (i=0;i<4;i++)
-	{
-		for (j=0;j<5;j++)
-		{
-			value = rows[1+i].cells[1+4-j].innerHTML;
-
-			rows[1+i].cells[1+4-j].className = "myLink";
-			rows[1+i].cells[1+4-j].onclick = function(){
-				var declCHARS = "NSEW";
-				var suitCHARS = "CDHSN";
-				var suit = $(this).index() - 1;
-				var schars = "cdhsn";
-				var suitChar = schars.charAt(suit);
-				var contract = "*" + suitCHARS.charAt(suit); // Put '*' in case it's number of tricks in makeable contracts table entry !
-				if (suit==4) contract = contract + "T"; // No Trumps
-				var declarer = $(this.parentNode).index() - 1;
-
-				var indx = (5*($(this.parentNode).index() - 1))+ ($(this).index() - 1);
-
-				g_showOriginalContract = false;
-
-				if ((g_defaultContract==0)||(indx!=g_defaultContractIndex))
-				{
-					setShowPlay(0);
-					playContract(declCHARS.charAt(declarer),suitChar,contract);
-				}
-				else
-					playLinContract();
-			};
-		}
-	}
-
-	showCredits();
-}
-
-function stopPlay()
-{
-	terminateSession();	// Terminate current playing session if there is one in progress.
-	setMode(0);			// Not in play mode any more
-
-	var contracts = document.getElementById("makeableContracts");
-	var rows = contracts.rows;
-
-	for (i=0;i<4;i++)
-	{
-		for (j=0;j<5;j++)
-		{
-			rows[1+i].cells[1+4-j].className = "mc";
-			rows[1+i].cells[1+4-j].onclick = null;
-		}
-	}
-
-	showCredits();
-}
 
 function selectQuadrant(index)
 {
